a178804e07871f87564f7a0c738a599fb1eaa9df
Veronica Carrillo-Marquez <veiko@f1v.co>
2018-01-13 19:13:10 -0500

    Using ESLint with AirBnb base rules

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,11 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: ['babel-loader', 'eslint-loader']
+      }
     ]
   }
 };
