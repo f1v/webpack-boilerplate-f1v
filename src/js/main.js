26b81efd60ced046fdab60ede13a0e85a8f5cd1b
Veronica Carrillo-Marquez <veiko@f1v.co>
2019-01-18 19:24:45 -0500

    🔣 chore(babel): Babel upgrade

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 // Testing Webpack bundle
-console.log('Boilerplate Started');
+console.log('Boilerplate Started'); // eslint-disable-line
 
 // Testing ES6
-setTimeout(() => console.log('Testing ES6'), 1000);
+setTimeout(() => console.log('Testing ES6'), 1000); // eslint-disable-line
 
 class TestClass {
   classBoundFunction = () => {
-    console.log('classBoundFunction called');
+    console.log('classBoundFunction called'); // eslint-disable-line
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var StyleLintPlugin = require('stylelint-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
   entry: ['./src/js/main.js', './src/css/main.scss'],
   output: {
-    filename: 'dist/bundle.js'
+    filename: 'dist/bundle.js',
   },
   module: {
     rules: [
@@ -13,25 +12,19 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           { loader: 'babel-loader' },
-          { loader: 'eslint-loader' }
-        ]
+          { loader: 'eslint-loader' },
+        ],
       },
       {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          use: [
-            { loader: 'css-loader' }, // translates CSS into CommonJS
-            { loader: 'sass-loader' } // compiles Sass to CSS
-          ],
-        }),
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          'css-loader',
+          'sass-loader',
+        ],
       },
     ],
   },
   plugins: [
-    new ExtractTextPlugin({
-      filename: 'dist/app.bundle.css',
-      allChunks: true,
-    }),
     new StyleLintPlugin({}),
   ],
 };
